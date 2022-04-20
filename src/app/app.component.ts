3ed720f2767b1a17ba94022995b8a4222580edc4
Manish Sharanappa <292166@axisbank.com>
2022-04-20 12:13:47 +0530

    API setup to add new user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-app';
+
+  constructor(private appService: ApiService) {}
+
+  userForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required)
+  });
+
+  
+  users: any = [];
+  userCount = 0;
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  onSubmit() {
+    this.appService.addUser(this.userForm.value).pipe(takeUntil(this.destroy$)).subscribe(data => {
+      console.log('message::::', data);
+      this.userCount = this.userCount + 1;
+      console.log(this.userCount);
+      this.userForm.reset();
+    });
+  }
+
+  getAllUsers() {
+    this.appService.getUsers().pipe(takeUntil(this.destroy$)).subscribe((users : any) => {
+        this.users = users;
+    });
+  }
+
 }
